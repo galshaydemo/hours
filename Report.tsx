024ad931d1d06e6ea8df827e8954909e4c66be06
unknown <galshay24@gmail.com>
2020-10-21 16:40:02 +0300

    add interface

---
+++ Report.tsx
@@ -0,0 +1,146 @@
+import React,{useState,useEffect} from 'react';
+import {
+  SafeAreaView,
+  StyleSheet,
+  ScrollView,
+  View,
+  Text,
+  StatusBar,
+  Image,
+  TouchableOpacity,
+  Button,
+} from 'react-native';
+import SQLite from "react-native-sqlite-storage";
+import DatePicker from './DatePicker';
+import TimePicker from './TimePicker';
+import messaging from '@react-native-firebase/messaging';
+import Geolocation from '@react-native-community/geolocation';
+import { FlatList } from 'react-native-gesture-handler';
+import { reportHour } from './interfaces';
+export default function Home()
+{
+    useEffect(function() {
+        SQLite.DEBUG(true);
+        SQLite.enablePromise(true);
+        
+        SQLite.openDatabase({
+            name: "TestDatabase",
+            location: "default"
+        }).then((db) => {
+          setDb(db)   
+          readData(db)         
+        });
+    }, []);
+    
+  
+    
+      
+  
+    
+    const [db, setDb] = useState(null);
+    const [data, setData] = useState([]);
+    const readData = (localDb) => {
+    var temp=[]
+    const sqlName="select s.day,s.month,s.year,s.hour as sHour,s.minute as sMinute,e.hour as eHour,e.minute as eMinute,(60.0*e.Hour + e.Minute -(60*s.Hour+s.Minute))/60.0 as hours,s.dayWeek  from (SELECT * FROM hours where status=0) s left join  (SELECT * FROM hours where status=1) e on e.day=s.day and e.month=s.month and e.year=s.year"
+      localDb.transaction(tx => {
+        
+        // sending 4 arguments in executeSql
+        tx.executeSql(sqlName, [], // passing sql query and parameters:null
+        (tx,results) => {
+          
+          for(let i=0;i<results.rows.length;i++)
+          {
+              console.log(i)
+              let tempObject=new reportHour();
+              console.log('row i'+i)
+              console.log(results.rows.item(i).sHour)
+              tempObject.index=i;
+              tempObject.key=results.rows.item(i).day.toString();
+              tempObject.day=results.rows.item(i).day.toString();
+              tempObject.year=results.rows.item(i).year;
+              tempObject.month=results.rows.item(i).month;
+              tempObject.sHour=results.rows.item(i).sHour;
+              tempObject.sMinute=results.rows.item(i).sMinute;
+              tempObject.eHour=results.rows.item(i).eHour;
+              tempObject.eMinute=results.rows.item(i).eMinute;
+              tempObject.dayWeek=results.rows.item(i).dayWeek;
+              tempObject.hours=results.rows.item(i).hours;
+              temp.push(tempObject)
+              console.log('after push')
+          }
+          console.log(temp)
+          setData(temp)
+        }
+          
+      ) // end transaction
+      })
+    }
+    
+  
+    return (<SafeAreaView>
+      <View style={styles.scrollView}>
+        <Text style={styles.sectionTitle}>דוח שעות</Text>
+        <FlatList data={data}  renderItem={({ item, index }) => {
+        return <View style={{flexDirection:'row',justifyContent:'space-between'}}>
+                <Text>{item.dayName()}</Text>
+                <Text>{item.timeStart()}</Text>
+                <Text>{item.timeEnd()}</Text>
+                <Text>{item.weekDayName()}</Text>
+                <Text>{item.hoursStr()}</Text>
+                </View>
+        }}></FlatList>
+      </View>
+      </SafeAreaView>
+      )
+}
+const styles = StyleSheet.create({
+    buttonTextStyle:
+    {
+      width:200,
+      borderWidth:0,
+    },
+    scrollView: {
+      
+    },
+    engine: {
+      position: 'absolute',
+      right: 0,
+    },
+    action:
+    {
+      marginBottom:20,
+      flexDirection:'row',justifyContent:'space-around'
+    },
+    img:{
+      
+    },
+    body: {
+      
+    },
+    sectionContainer: {
+      marginTop: 32,
+      paddingHorizontal: 24,
+    },
+    sectionTitle: {
+      fontSize: 24,
+      fontWeight: '600',
+      
+    },
+    sectionDescription: {
+      marginTop: 8,
+      fontSize: 18,
+      fontWeight: '400',
+      
+    },
+    highlight: {
+      fontWeight: '700',
+    },
+    footer: {
+      
+      fontSize: 12,
+      fontWeight: '600',
+      padding: 4,
+      paddingRight: 12,
+      textAlign: 'right',
+    },
+  });
---
+++ interfaces.tsx
@@ -0,0 +1,60 @@
+
+
+export class reportHour
+{
+    index:number;
+    key:string;
+    day:number;
+    year:number;
+    month:number;
+    sHour:number;
+    sMinute:number;
+    eHour:number;
+    eMinute:number;
+    dayWeek:number;
+    weekDayNames=['א','ב','ג','ד','ה','ו','ש'];
+    hours:number;
+    dayName() {
+        let str='';
+        if ( this.day < 10  )  str+='0';
+        str+=this.day.toString(); 
+        str+="-";
+        if ( this.month < 10  )  str+='0';
+        str+=this.month.toString(); 
+        str+="-";
+        str+=this.year.toString(); 
+        return str;
+        //return (this.day < 10 ) ? '0' + this.day.toString() : this.day.toString() +'-'+ (this.month < 10 ) ? '0' +this.month.toString(): this.month.toString()+'-'+this.year.toString()   
+    }
+    formatTime(hour:number,minute:number)
+    {
+        let str:string='';
+        if ( hour < 10 ) str+='0';
+        str+=hour.toString(); 
+        str+=":";
+        if ( minute < 10 ) str+='0';
+        str+=minute.toString(); 
+        return str
+
+    }
+    timeStart() {
+        return this.formatTime(this.sHour,this.sMinute)
+    }
+    timeEnd() {
+        if ( this.eHour )
+        return this.formatTime(this.eHour,this.eMinute)
+        return '00:00'
+    }
+    weekDayName()
+    {
+        return this.weekDayNames[this.dayWeek]
+    }
+    hoursStr()
+    {
+        if ( this.hours)
+        return this.hours.toFixed(2);
+        return '0.00';
+    }
+
+
+}
